3087658e473a6d9882305522fc8ffa4a1a570c92
GurhanAksoy <aksoy75@gmail.com>
2025-07-23 16:22:23 +0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -40,15 +40,16 @@ export default function Home() {
           {/* Modern WhatsApp Butonu */}
           <div className="mt-6">
             <a
-              href="https://wa.me/905304066686"
-              target="_blank"
-              className="inline-flex items-center gap-2 bg-green-500 hover:bg-green-600 text-white font-medium px-6 py-3 rounded-full shadow-lg transition-transform transform hover:scale-105"
-            >
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 448 512" fill="currentColor">
-                <path d="M380.9 97.1C339-6.2 217.1-31.8 123.2 10.1 58.7 40.5 19.8 108.5 21.3 181.5c1.1 54.8 20.1 105.5 53.2 144.5L49.1 470.6c-3.5 13.5 9.2 25.2 22.5 20.5l123.6-45.2c35.2 12.6 73.8 15.4 111.3 6.5 99.2-24.2 163.8-121.7 145.2-221.3-5.8-30.2-19.7-57.8-38.8-78.4zM224 439.7c-30.9.1-61.2-7.5-88.4-22L98.2 429l18.1-65.5c-20.5-27.5-32.5-61.4-33.1-96.3-.9-83.3 66.6-151.4 149.9-152.3s151.4 66.6 152.3 149.9c.9 83.3-66.6 151.4-149.9 152.3z" />
-              </svg>
-              WhatsApp Bildirim Gönder
-            </a>
+  href="https://wa.me/905304066686"
+  target="_blank"
+  className="inline-flex items-center gap-2 bg-green-500 hover:bg-green-600 px-6 py-3 rounded-full shadow-lg transition-transform transform hover:scale-105"
+>
+  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-white" viewBox="0 0 448 512" fill="currentColor">
+    <path d="M380.9 97.1C339-6.2 217.1-31.8 123.2 10.1 58.7 40.5 19.8 108.5 21.3 181.5c1.1 54.8 20.1 105.5 53.2 144.5L49.1 470.6c-3.5 13.5 9.2 25.2 22.5 20.5l123.6-45.2c35.2 12.6 73.8 15.4 111.3 6.5 99.2-24.2 163.8-121.7 145.2-221.3-5.8-30.2-19.7-57.8-38.8-78.4zM224 439.7c-30.9.1-61.2-7.5-88.4-22L98.2 429l18.1-65.5c-20.5-27.5-32.5-61.4-33.1-96.3-.9-83.3 66.6-151.4 149.9-152.3s151.4 66.6 152.3 149.9c.9 83.3-66.6 151.4-149.9 152.3z" />
+  </svg>
+  <span className="text-white font-semibold">WhatsApp Bildirim Gönder</span>
+</a>
+
           </div>
         </section>
 
